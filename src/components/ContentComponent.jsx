9b377625c65c9a9689d40b2f0cb5f1232fd7c522
sulphite <mikanchik@gmail.com>
2024-08-22 17:26:22 +0100

    change component to grid layout

--- src/components/ContentComponent.jsx
+++ src/components/ContentComponent.jsx
@@ -27,8 +27,9 @@ const ContentComponent = ({data, setExpanded}) => {
   }
 
   return (
-    <div className="container">
-      <img src={rewind} className="expandedview__back" alt="back" onClick={back} />
+    <>
+    <img src={rewind} className="expandedview__back" alt="back" onClick={back} />
+    <div className="grid-container">
       {/* Poster section */}
       {data.posters &&
       <section className="posterWrapper">
@@ -38,16 +39,17 @@ const ContentComponent = ({data, setExpanded}) => {
       </section>}
 
       {/* Rectangle w.text */}
-      <div className="rectangle">
+      <div className="side">
         <h1>{data.title}</h1>
         <h2>Insight</h2>
         <h3>{data.insight}</h3>
         <h2>SMP</h2>
         <h4>{data.smp}</h4>
+      </div>
 
         {/* Section for TV video */}
-        {true &&
-          <section>
+        {data.tvAdvert &&
+          <section className="wide">
             <h2>30 second TV ad</h2>
             <div className="video"></div>
           </section>
@@ -55,68 +57,78 @@ const ContentComponent = ({data, setExpanded}) => {
 
         {/* Section for activation */}
 
-        {data.activation && <section>
-          <h2>Activation</h2>
-          <p>
-            Scunthorpe United are set to play Liverpool in the FA Cup, and need
-            to smell unflappable in the face of such a big challenge.
-            <br />
-            <br />
-            So, Brut are going to sponsor Scunthorpe’s finest for as long as
-            they can survive the tournament, and provide them with kits that are
-            scented with the Brut Original fragrance.
-            <br />
-            <br />
-            Proving that Brut helps you smell unflappable—even if you’re
-            screaming on the inside.
-            <br />
-          </p>
+        {data.activation && 
+        <>
           <img className="activation" src="" alt="Activation" />
-        </section>}
+          <section className="side">
+            <h2>Activation</h2>
+            <p>
+              Scunthorpe United are set to play Liverpool in the FA Cup, and need
+              to smell unflappable in the face of such a big challenge.
+              <br />
+              <br />
+              So, Brut are going to sponsor Scunthorpe’s finest for as long as
+              they can survive the tournament, and provide them with kits that are
+              scented with the Brut Original fragrance.
+              <br />
+              <br />
+              Proving that Brut helps you smell unflappable—even if you’re
+              screaming on the inside.
+              <br />
+            </p>
+          </section>
+        </>}
 
         {/* Section for Social Media */}
-        <section>
+        {data.socialMedia && 
+        <>
+        {data.sticker ? <img src={data.sticker} style={{maxWidth: "500px", margin: "0 auto"}}/> : <div></div>}
+        <section className="side">
           <h2>Social Media</h2>
           <p>Hello World!</p>
-          <div className="socialMedia"></div>
+          <img className="socialMedia" src={data.socialMedia} />
         </section>
+        </>}
 
         {/* Section for Radio Ad */}
-        <section>
-          <h2>60 second radio ad:</h2>
-          {data.radioAd && 
-            <audio controls>
-              <source src={data.radioAd} type="audio/mpeg" />
-            </audio>
-          }
-          <p>User (Thick East End accent, hushed tone):</p>
-          <p className="scriptBold">‘ere mate, can I eh... Can I get 36g of the finest?</p>
-          <p>Dealer:</p>
-          <p className="scriptBold">Sure.</p>
-          <p>User:</p>
-          <p className="scriptBold">Woar, that’s the stuff.</p>
-          <p>Dealer (Sounding bemused): </p>
-          <p className="scriptBold">Um, you got payment?</p>
-          <p>User (Sounding transfixed):</p>
-          <p className="scriptBold">Eh? Oh yeah,‘ere you go.</p>
-          <p>
-            (Sound of a card machine beeping communicating that he’s actually
-            bought the Wotsits from a shop).
-          </p>
-          <p>User (Sounding transfixed):</p>
-          <p className="scriptBold">Ta.</p>
-          <p>Dealer (Sounding a little judgemental):</p>
-          <p className="scriptBold">See you later.</p>
-          <p>
-            (Sound of Wotsits bag being ripped open and the sound of snacks
-            being wolfed down by the user as he moans in ecstasy).
-          </p>
-          <p>V/O (A whispering East End accent):</p>
-          <p className="scriptBold">Wotsits. Addictively Cheesy.</p>
-        </section>
+        {data.radioAd && 
+        <>
+          <img src="/src/assets/temporaryRadio.jpeg" /> 
+          <section className="side">
+            <h2>60 second radio ad:</h2>
+              <audio controls>
+                <source src={data.radioAd} type="audio/mpeg" />
+              </audio>
+            
+            <p>User (Thick East End accent, hushed tone):</p>
+            <p className="scriptBold">‘ere mate, can I eh... Can I get 36g of the finest?</p>
+            <p>Dealer:</p>
+            <p className="scriptBold">Sure.</p>
+            <p>User:</p>
+            <p className="scriptBold">Woar, that’s the stuff.</p>
+            <p>Dealer (Sounding bemused): </p>
+            <p className="scriptBold">Um, you got payment?</p>
+            <p>User (Sounding transfixed):</p>
+            <p className="scriptBold">Eh? Oh yeah,‘ere you go.</p>
+            <p>
+              (Sound of a card machine beeping communicating that he’s actually
+              bought the Wotsits from a shop).
+            </p>
+            <p>User (Sounding transfixed):</p>
+            <p className="scriptBold">Ta.</p>
+            <p>Dealer (Sounding a little judgemental):</p>
+            <p className="scriptBold">See you later.</p>
+            <p>
+              (Sound of Wotsits bag being ripped open and the sound of snacks
+              being wolfed down by the user as he moans in ecstasy).
+            </p>
+            <p>V/O (A whispering East End accent):</p>
+            <p className="scriptBold">Wotsits. Addictively Cheesy.</p>
+          </section>
+        </>}
 
         {/* Section for Topicals */}
-        <section>
+        <section className="wide">
           <h2>Topicals</h2>
           <div className="topicalsWrapper">
             <div className="topicalPoster" src="Last min"></div>
@@ -129,7 +141,7 @@ const ContentComponent = ({data, setExpanded}) => {
           </div>
         </section>
       </div>
-    </div>
+    </>
   );
 };
 
